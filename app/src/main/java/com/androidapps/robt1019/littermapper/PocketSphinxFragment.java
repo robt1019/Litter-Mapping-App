b61ee61171caffb54d7f69b01dfbfb7b3fa5b9cd
robt1019 <robert.i.taylor88@gmail.com>
2015-06-28 21:33:17 +0100

    test commit

--- app/src/main/java/com/androidapps/robt1019/littermapper/PocketSphinxFragment.java
+++ app/src/main/java/com/androidapps/robt1019/littermapper/PocketSphinxFragment.java
@@ -291,7 +291,7 @@ public class PocketSphinxFragment extends Fragment implements
     private void setupRecognizer(File assetsDir) throws IOException {
         recognizer = defaultSetup()
                 .setAcousticModel(new File(assetsDir, "en-us-ptm"))
-                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))ls
+                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
 
                         // To disable logging of raw audio comment out this call
                 .setRawLogDir(assetsDir)
